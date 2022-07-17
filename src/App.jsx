b8cebd2912a9e3b27ef07cf4fde284281feb44c9
Stuard Quiroz <stuuard96@gmail.com>
2022-07-17 10:58:13 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,86 @@
+import { useEffect } from "react";
+import "./App.css";
+
+export const App = () => {
+  useEffect(() => {
+    (function (src) {
+      var a = document.createElement("script");
+      a.type = "text/javascript";
+      a.async = true;
+      a.src = src;
+      var b = document.getElementsByTagName("script")[0];
+      b.parentNode.insertBefore(a, b);
+    })("//sandbox.tinypass.com/xbuilder/experience/load?aid=PeVZORGJsu");
+
+    // const tp=window.tp || [];
+    // tp.push(["setCookieDomain", ""])
+  }, []);
+
+  const PianoLogin = () => {
+    console.log("login");
+    const tp = window.tp || [];
+    tp.push([
+      "init",
+      function () {
+        tp.pianoId.show({
+          disableSignUp: false,
+          displayMode: "modal",
+          screen: "register",
+          containerSelector: "#login-form",
+          loggedIn: function (data) {
+            console.log("user", data.user, "logged in with token", data.token);
+          },
+
+          loggedOut: function () {
+            console.log("user logged out");
+          },
+        });
+      },
+    ]);
+  };
+
+  const PianoLogout = () => {
+    const tp = window.tp || [];
+    tp.push([
+      "init",
+      function () {
+        console.log("logout");
+        tp = window.tp || [];
+        tp.pianoId.logout();
+        //reload the page
+        window.location.reload();
+      },
+    ]);
+  };
+
+  const PianoProfile = () => {
+    const tp = window["tp"] || [];
+    tp.push([
+      "init",
+      function () {
+        tp.myaccount.show({
+          displayMode: "inline",
+          containerSelector: "#my-account",
+        });
+      },
+    ]);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Bienvenido a mi sitio</p>
+        <a
+          className="App-link"
+          href="#"
+          rel="noopener noreferrer"
+          onClick={PianoLogin}
+        >
+          Iniciar
+        </a>
+        <button onClick={PianoProfile}>Mi Perfil</button>
+        <button onClick={PianoLogout}>Logout</button>
+      </header>
+    </div>
+  );
+};
